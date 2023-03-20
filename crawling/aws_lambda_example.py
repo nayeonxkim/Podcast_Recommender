d67add1b20834c8c6763e9e89f7cccd8b5ba28f3
nayeonkim <kny0628m@gmail.com>
2023-03-20 17:56:08 +0900

    230320 start PJT

---
+++ crawling/aws_lambda_example.py
@@ -0,0 +1,10 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = 'https://www.podbbang.com/channel-categories'
+soup = BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")
+a_tags = soup.find_all('a')
+result_list = []
+for i in a_tags:
+    result_list.append(i.get_text())
+print(result_list)
